be378375a1b58db029a026bc12463a8d48699551
Sona <127790511+Sona-Yadav@users.noreply.github.com>
2024-10-13 20:21:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,12 @@ resource "google_storage_bucket" "my-bucket" {
   force_destroy            = true
   public_access_prevention = "enforced"
 }
+
+
+resource "google_storage_bucket" "my-bucket2" {
+  name                     = "import-test-bucket-terarform"
+  project                  = "test-terraform-438504"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
