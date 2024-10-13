025db062424b96943ca215a08a42ac0874c103e7
Sona <127790511+Sona-Yadav@users.noreply.github.com>
2024-10-13 20:30:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "google_storage_bucket" "my-bucket" {
 }
 
 
-resource "google_storage_bucket" "my-bucket2" {
+resource "google_storage_bucket" "my-bucket25" {
   name                     = "import-test-bucket-terarform"
   project                  = "test-terraform-438504"
   location                 = "US"
