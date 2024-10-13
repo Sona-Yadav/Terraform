9583f07b2bf8eb034d6783e2b90c618eae793c68
Sona <127790511+Sona-Yadav@users.noreply.github.com>
2024-10-13 19:22:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,13 @@ resource "google_storage_bucket" "my-bucket" {
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
-  depends_on = "google_storage_bucket" "my-bucket"
+  depends_on = google_storage_bucket.my-demobucket
 }
 
-resource "google_storage_bucket" "my-bucket" {
+resource "google_storage_bucket" "my-demobucket" {
   name                     = "sona-demogcp978"
   project                  = "test-terraform-438504"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
+}
