2603a6a0757841be5242fb0ab30410699788c0a3
Sona <127790511+Sona-Yadav@users.noreply.github.com>
2024-10-13 19:17:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,55 +1,15 @@
 resource "google_storage_bucket" "my-bucket" {
-  name                     = "sona-demogcp1256"
+  name                     = "sona-demogcp789"
   project                  = "test-terraform-438504"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
+  depends_on = "google_storage_bucket" "my-bucket"
 }
 
-resource "google_service_account" "default" {
-  account_id   = "my-custom-sa"
-  display_name = "Custom SA for VM Instance"
-}
-
-resource "google_compute_instance" "default" {
-  name         = "my-instance"
-  machine_type = "n2-standard-2"
-  zone         = "us-central1-a"
-
-  tags = ["foo", "bar"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        my_label = "value"
-      }
-    }
-  }
-
-  // Local SSD disk
-  scratch_disk {
-    interface = "NVME"
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-
-  metadata = {
-    foo = "bar"
-  }
-
-  metadata_startup_script = "echo hi > /test.txt"
-
-  service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.default.email
-    scopes = ["cloud-platform"]
-  }
-}
-
+resource "google_storage_bucket" "my-bucket" {
+  name                     = "sona-demogcp978"
+  project                  = "test-terraform-438504"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
